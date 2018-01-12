443f46f966fd0e882922f75e2a545c083dbf540c
Nick <hospitalqqqq@gmail.com>
2018-01-12 14:35:31 +0300

    5attemp

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,49 +3,10 @@ require File.dirname(__FILE__) + '/../rails_helper'
 describe Card do
   let(:card) {create :card}
 
-  it "should be valid" do
-    expect(card).to be_valid
-  end
-
-  it "is not valid without original_text and tranlsated_text" do
-    card.original_text=''
-    card.translated_text=''
-    expect(card).to_not be_valid
-  end
-
-  it "is not valid without a original_text " do
-    card.translated_text='Anton'
-    card.original_text=''
-    expect(card).to_not be_valid
-  end
-
-  it "is not valid without a translated_text " do
-    card.original_text='Anton'
-    card.original_text=''
-    expect(card).to_not be_valid
-  end
-
-  it "is valid without incorrect input " do
-    card.original_text='fAtHer'
-    card.translated_text='FuThEr'
-    expect(card).to be_valid
-  end
-
-  it "is not valid with numbers in translated_text" do
-    card.translated_text='122'
-    expect(card).to_not be_valid
-  end
-
-  it "is not valid with numbers in original_text" do
-    card.original_text='122'
-    expect(card).to_not be_valid
-  end
-
-  it "is not valid with numbers in both" do
-    card.original_text='122'
-    card.translated_text='122'
-    expect(card).to_not be_valid
-  end
+  it "is true if update_review_date work" do
+    card.update_review_date
+    expect(card.review_date).to eq(Date.today+3.days)
+    end
 
   it "is true if set_review_date work" do
     @card=card.review_date
