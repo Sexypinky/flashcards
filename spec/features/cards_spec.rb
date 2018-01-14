5b8d35d8c780a1fa57612ecf63ca5649ec0cc48f
Nick <hospitalqqqq@gmail.com>
2018-01-14 13:17:18 +0300

    final attemp

--- spec/features/cards_spec.rb
+++ spec/features/cards_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 context "can use flashcards form" do
+  let(:card) {create :card}
 
   before(:each) do
     visit root_path
-    fill_in "check_text", with: "Something"
-    click_button "Проверить"
   end
 
   it "right checking card" do
+    fill_in "check_text", with: "Something"
+    click_button "Проверить"
     expect(page).to have_content "Слово переведено неверно"
   end
 
   it "wrong checking card" do
+    fill_in "check_text", with: "Apple"
+    click_button "Проверить"
     expect(page).not_to have_content "Cлово переведено верно"
   end
 end
