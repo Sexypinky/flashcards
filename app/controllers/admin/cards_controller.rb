d2a44b145ecf34224335ceab9e71baa3bdf2abd8
Nick <hospitalqqqq@gmail.com>
2018-01-19 17:52:46 +0300

    1attemp

--- app/controllers/admin/cards_controller.rb
+++ app/controllers/admin/cards_controller.rb
@@ -5,6 +5,6 @@ class Admin::CardsController <  Admin::AdminController
   end
 
   def card_params
-    params[:card].permit(:original_text, :translated_text, :review_date)
+    params[:card].permit(:original_text, :translated_text, :review_date, :image, :remove_image, :remote_image_url )
   end
 end
