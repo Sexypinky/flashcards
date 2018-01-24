808dded940179df17a30b674542ec4f83aa816bc
sexypinky <hospitalqqqq@gmail.com>
2018-01-24 12:50:41 +0300

    make it better

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,11 @@ class StaticPagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    unless current_user.actual_deck
+    if not current_user.actual_deck.nil?
     @card=current_user.actual_deck.cards.check_date.first
+    if @card.blank?
+      redirect_to decks_path, notice:'Нет доступных карточек для просмотра'
+    end
      else
       @card=current_user.decks.sample.cards.check_date.first
       if @card.blank?
