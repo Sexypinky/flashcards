0ca163532cd1cf2ecdee61bfb03469f9b2d644b9
sexypinky <hospitalqqqq@gmail.com>
2018-02-12 14:34:04 +0300

    make it better

--- db/migrate/20180125063405_add_level_to_card.rb
+++ db/migrate/20180125063405_add_level_to_card.rb
@@ -2,7 +2,6 @@ class AddLevelToCard < ActiveRecord::Migration[5.1]
   def change
     change_table :cards do |t|
       t.integer :level, default: 0
-      t.integer :try, default: 0
     end
   end
 end
