481fa7cb6d2c41e8a31ea5895f37f4dc0b999995
sexypinky <hospitalqqqq@gmail.com>
2018-02-17 08:05:08 +0300

    make it better

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
