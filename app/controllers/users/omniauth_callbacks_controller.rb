f03a624046c01ec20182f71bf52a1f32ac6020bd
sexypinky <hospitalqqqq@gmail.com>
2018-02-17 08:12:51 +0300

    make it better

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
-    if request.env["omniauth.auth"].info.email.blank?
-      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
-      return
-    end
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
