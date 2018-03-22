8324d2f5a99e693b6b2dc200ec7213a2364111ac
sexypinky <hospitalqqqq@gmail.com>
2018-03-22 19:30:44 +0300

    make it better

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ module Flashcards
 
     I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
 
-    config.i18n.available_locales = [:en, :ru]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
